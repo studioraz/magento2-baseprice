2033a1cd9825d63f2be1509ee73e1c944a2fb8f4
Maksym <maxim@studioraz.co.il>
2024-11-04 07:32:14 +0200

    VINO-298 added product unit to render with unit amount on PDP

--- Plugin/ProductAttributes.php
+++ Plugin/ProductAttributes.php
@@ -6,10 +6,12 @@ use Magento\Catalog\Block\Product\View\Attributes;
 class ProductAttributes
 {
 
-    private const BASE_PRICE_AMOUNT = 'baseprice_product_amount';
+    private const BASE_PRICE_PRODUCT_AMOUNT = 'baseprice_product_amount';
+    private const BASE_PRICE_PRODUCT_UNIT = 'baseprice_product_unit';
 
     /**
-     * After plugin for getAdditionalData
+     * AFTER PLUGIN
+     * @see \Magento\Catalog\Block\Product\View\Attributes::getAdditionalData()
      *
      * @param Attributes $subject
      * @param array $result
@@ -18,11 +20,27 @@ class ProductAttributes
     public function afterGetAdditionalData(Attributes $subject, array $result)
     {
         foreach ($result as $attributeCode => $attributeValue) {
-            if ($attributeCode == self::BASE_PRICE_AMOUNT) {
-                $result[$attributeCode]['value'] = intval($attributeValue['value']);
+            if ($attributeCode == self::BASE_PRICE_PRODUCT_AMOUNT) {
+                $productAmount = intval($attributeValue['value']);
+                $productUnit = $this->getProductUnit($subject);
+                $result[$attributeCode]['value'] = $productAmount . $productUnit;
             }
         }
 
         return $result;
+    }
+
+    /**
+     * @param $subject
+     * @return string
+     */
+    protected function getProductUnit($subject): string
+    {
+        $unit = '';
+        if ($subject->getRequest()->getFullActionName() == 'catalog_product_view') {
+            $product = $subject->getProduct();
+            $unit = ' ' . $product->getAttribute(self::BASE_PRICE_PRODUCT_UNIT);
+        }
+        return $unit;
     }
 }
